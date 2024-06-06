ed8c42d42b39c3df0953b5a91dce16d34513169e
Thiago Silva <thiago_als@hotmail.com>
2024-06-06 00:42:58 -0300

    Adicionando projeto

---
+++ src/main/java/com/redes/cotacao_api/CotacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.redes.cotacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CotacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CotacaoApiApplication.class, args);
+	}
+
+}
